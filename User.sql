f2561d672eb5541acfdef98ec83926624e2f8105
Roger <nguyenvo1028rg@gmail.com>
2021-12-06 16:42:35 +0700

    Adding security code

--- User.sql
+++ User.sql
@@ -1,6 +1,29 @@
-CREATE LOGIN Lannest
-	with PASSWORD = '1';
+--CREATE LOGIN Nguyen
+--	with PASSWORD = '1';
+--GO
+
+--CREATE USER Nguyen FOR LOGIN Nguyen;
+
+
+--CREATE LOGIN Khang
+--	with PASSWORD = '1';
+--GO
+
+--CREATE USER Khang FOR LOGIN Khang;
+--GO
+
+--CREATE LOGIN Khoa
+--	with PASSWORD = '1';
+--GO
+
+--CREATE USER Khoa FOR LOGIN Khoa;
+--GO
+
+sp_addrolemember @rolename = NGUOIPHANBIEN, @membername = Nguyen;
 GO
 
-CREATE USER Lannest FOR LOGIN Lannest;
+sp_addrolemember @rolename = BIENTAPVIEN, @membername = Khang;
+GO
+
+sp_addrolemember @rolename = TACGIA, @membername = Khoa;
 GO
